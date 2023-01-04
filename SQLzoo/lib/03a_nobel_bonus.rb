fe70b63790b6afeeeae336d989cefa4ea8a938e6
Aaron Jung <aaronjung3@gmail.com>
2023-01-03 17:59:36 -0800

    W5D2 Classwork

---
+++ SQLzoo/lib/03a_nobel_bonus.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+    SELECT yr
+    FROM nobels
+    WHERE
+    EXISTS
+      (SELECT
+        yr
+      FROM
+        nobels
+      WHERE
+        subject = 'Physics')
+    AND
+
+    NOT EXISTS
+      (SELECT
+          yr
+        FROM
+          nobels
+        WHERE
+          subject = 'Chemistry')
+  SQL
+end
